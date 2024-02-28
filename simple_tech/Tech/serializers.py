b3ef7aac29aa7a7c5d8f1d989b4603c83e05f5ee
krjprogrammer <137873227+krjprogrammer@users.noreply.github.com>
2024-02-28 11:21:25 +0530

    Delete simple_tech directory

--- simple_tech/Tech/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Company
-class company_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = '__all__'
--- simple_tech/Tech/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import post_company_data,comapany_api_view
-urlpatterns = [
-    path('post_company_data',post_company_data.as_view()),
-    path('company_api_view',comapany_api_view.as_view())
-]
